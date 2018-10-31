4a2d1ae46b4d7c0378ea18b2ac133caa78fb0587
江珮妤 <Luxgen077@gmail.com>
2018-10-31 17:34:41 +0800

    練習一:基礎路由參數(修正)

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('student/{student_no}', function ($student_no) {
     return "學號:".$student_no;
 });
 
-Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject) {
+Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject) {
     return "學號:".$student_no."的".$subject."成績";
 });
