1e7d7af8503400cf5c5371931c8723ed43409bb2
江珮妤 <Luxgen077@gmail.com>
2018-10-31 17:22:30 +0800

    練習一:基礎路由參數

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('student/{student_no}', function ($student_no) {
+    return "學號:".$student_no;
+});
+
+Route::get('student/{student_no}/score/{subject?}',function($student_no,$subject) {
+    return "學號:".$student_no."的".$subject."成績";
 });
