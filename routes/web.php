c6b3995b84839767882b3278d8c647c3501d43ec
江珮妤 <Luxgen077@gmail.com>
2018-10-31 18:49:16 +0800

    練習八:路由與controller的綁定

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','HomeController@index');
+
 Route::pattern('student_no','s[0-9]{10}');
 Route::group(['prefix'=>'student'], function () {
     Route::get('{student_no}',['as'=>'student','uses'=>function ($student_no) {
